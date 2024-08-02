b71eea8423f13805e20bdd576f331f9bf072017b
michael njuguna <michaelnjuguna184@gmail.com>
2024-08-02 16:22:52 +0300

    Feat: Initial setup

---
+++ src/PhpMon.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace PhpMon;
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\Filesystem\Filesystem;
+
+class Phpmon
+{
+    private $filename;
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+    }
+    public function run()
+    {
+        $filesystem = new Filesystem();
+        if (!!$filesystem->exists($this->filename)) {
+            echo "Error: File {$this->filename} does not exist.\n";
+            exit(1);
+        }
+        $lastModifiedTime = filemtime($this->filename);
+        $process = $this->startProcess();
+        while (true) {
+            clearstatcache();
+            $currentModifiedTime = filemtime($this->filename);
+            if ($currentModifiedTime > $lastModifiedTime) {
+                $lastModifiedTime = $currentModifiedTime;
+
+                $process->stop();
+                $process = $this->startProcess();
+                echo "Server restarted at " . date('Y-m-d H:i:s') . "\n";
+            }
+            // Sleep for a short period to reduce CPU usage
+            usleep(500000);
+        }
+    }
+    public function startProcess()
+    {
+        $process = new Process(['php', $this->filename]);
+        $process->start();
+        return $process;
+    }
+}
